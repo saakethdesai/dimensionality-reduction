867ec51af6bfc41f5fa441ae9715402d40049540
Saaketh Desai <saadesa@S1072476.local>
2023-10-30 10:40:11 -0600

    Updated PCA scripts

--- models/pca/dendrite/eval.py
+++ models/pca/dendrite/eval.py
@@ -21,6 +21,19 @@ train_data = hf['train'][:]
 #test
 test_data = hf['test'][:]
 
+data = np.vstack((train_data, test_data))
+print (train_data.shape, test_data.shape, data.shape)
+
+#read shuffled data to retrain
+hdffile = "/qscratch/saadesa/dimensionality_reduction/final_dataset/dendrite_512.h5"
+hf = h5py.File(hdffile, 'r')
+
+print ("Reading data")
+#train
+train_data = hf['train'][:] 
+#test
+test_data = hf['test'][:]
+
 print ("Done reading data")
 
 print (train_data.shape, test_data.shape)
@@ -35,34 +48,21 @@ for ncomp in ncomp_list:
     pca_model = PCA(n_components=ncomp)
     pca_model.fit(train_data)
 
-    train_z = pca_model.transform(train_data)
-    test_z = pca_model.transform(test_data)
+    z = pca_model.transform(data)
     
-    print ("TRANSFORMED DATA = ", test_z.shape)
-    test_recon = pca_model.inverse_transform(test_z)
-    print ("RECON DATA = ", test_recon.shape)
+    print ("TRANSFORMED DATA = ", z.shape)
+    recon = pca_model.inverse_transform(z)
+    print ("RECON DATA = ", recon.shape)
 
     #save arrays
-    np.save("dendrite_"+str(ncomp)+"_pca", test_recon)
-    np.save("dendrite_"+str(ncomp)+"_pca_z", test_z)
-    np.save("dendrite_"+str(ncomp)+"_pca_eigenvec", pca_model.components_)
-    np.save("dendrite_"+str(ncomp)+"_pca_eigenval", pca_model.explained_variance_)
+    np.save("dendrite_unshuffled"+str(ncomp)+"_pca", recon)
+    np.save("dendrite_unshuffled"+str(ncomp)+"_pca_z", z)
+    np.save("dendrite_unshuffled"+str(ncomp)+"_pca_eigenvec", pca_model.components_)
+    np.save("dendrite_unshuffled"+str(ncomp)+"_pca_eigenval", pca_model.explained_variance_)
 
     #compute MSE
-    mse = np.mean((test_data - test_recon)**2)
+    mse = np.mean((data - recon)**2)
     print (mse)
 
-    #save images
-    counter = 0
-    for i in range(test_data.shape[0]):
-        if (counter < 100):
-            data_gt = test_data[i].reshape((512, 512)) 
-            data_recon = test_recon[i].reshape((512, 512))
-            #plotting 
-            fig, (ax1, ax2) = plt.subplots(1, 2)
-            ax1.imshow(data_gt)
-            ax2.imshow(data_recon)
-            plt.savefig("test"+"_"+str(ncomp)+"_"+str(i)+".png")
-            plt.close()
-            counter += 1
+
 
--- models/pca/pvd/eval.py
+++ models/pca/pvd/eval.py
@@ -12,6 +12,20 @@ import h5py
 
 home_dir = os.getcwd()
 
+hdffile = "/qscratch/saadesa/dimensionality_reduction/final_dataset/pvd_unshuffled_data.h5"
+hf = h5py.File(hdffile, 'r')
+
+print ("Reading data")
+data = hf['data'][:] 
+print ("Done reading data")
+data = data.reshape((data.shape[0], data.shape[1]*data.shape[2]))
+print (data.shape)
+
+transformed_data = []
+global_mse = []
+
+
+#read shuffled data to retrain
 hdffile = "/qscratch/saadesa/dimensionality_reduction/final_dataset/pvd_data.h5"
 hf = h5py.File(hdffile, 'r')
 
@@ -38,34 +52,18 @@ for ncomp in ncomp_list:
     pca_model = PCA(n_components=ncomp)
     pca_model.fit(train_data)
 
-    train_z = pca_model.transform(train_data)
-    test_z = pca_model.transform(test_data)
+    z = pca_model.transform(data)
     
-    print ("TRANSFORMED DATA = ", test_z.shape)
-    test_recon = pca_model.inverse_transform(test_z)
-    print ("RECON DATA = ", test_recon.shape)
+    print ("TRANSFORMED DATA = ", z.shape)
+    recon = pca_model.inverse_transform(z)
+    print ("RECON DATA = ", recon.shape)
 
     #save arrays
-    np.save("pvd_"+str(ncomp)+"_pca", test_recon)
-    np.save("pvd_"+str(ncomp)+"_pca_z", test_z)
-    np.save("pvd_"+str(ncomp)+"_pca_eigenvec", pca_model.components_)
-    np.save("pvd_"+str(ncomp)+"_pca_eigenval", pca_model.explained_variance_)
+    np.save("pvd_unshuffled"+str(ncomp)+"_pca", recon)
+    np.save("pvd_unshuffled"+str(ncomp)+"_pca_z", z)
+    np.save("pvd_unshuffled"+str(ncomp)+"_pca_eigenvec", pca_model.components_)
+    np.save("pvd_unshuffled"+str(ncomp)+"_pca_eigenval", pca_model.explained_variance_)
 
     #compute MSE
-    mse = np.mean((test_data - test_recon)**2)
+    mse = np.mean((data - recon)**2)
     print (mse)
-
-    #save images
-    counter = 0
-    for i in range(test_data.shape[0]):
-        if (counter < 100):
-            data_gt = test_data[i].reshape((256, 256)) 
-            data_recon = test_recon[i].reshape((256, 256))
-            #plotting 
-            fig, (ax1, ax2) = plt.subplots(1, 2)
-            ax1.imshow(data_gt)
-            ax2.imshow(data_recon)
-            plt.savefig("test"+"_"+str(ncomp)+"_"+str(i)+".png")
-            plt.close()
-            counter += 1
-
